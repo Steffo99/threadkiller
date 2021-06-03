146c2cf2a8ed3e198140d208ab85a1cd2617d2ed
Stefano Pigozzi <me@steffo.eu>
2021-06-03 14:57:16 +0200

    And fix a bug.

--- threadkiller/__main__.py
+++ threadkiller/__main__.py
@@ -4,14 +4,18 @@ from telegram.ext import Filters
 
 
 def yeetus_deletus(update: telegram.Update, _context: telegram.ext.CallbackContext):
-    update.message.reply_text('⚠️ To send messages in this group, please Comment on posts of the associated'
-                              ' channel.')
+    try:
+        update.message.reply_text('⚠️ To send messages in this group, please Comment on posts of the associated'
+                                  ' channel.')
+    except telegram.error.RetryAfter:
+        pass
     update.message.delete()
 
 
 updater = telegram.ext.Updater(token=os.environ["TG_TOKEN"])
 updater.dispatcher.add_handler(
-    telegram.ext.MessageHandler(~Filters.reply & Filters.group & ~Filters.status_update, yeetus_deletus)
+    telegram.ext.MessageHandler(~Filters.reply & Filters.group & ~Filters.user(777000) & ~Filters.status_update,
+                                yeetus_deletus)
 )
 
 
