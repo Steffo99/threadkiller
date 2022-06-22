cc241b70b03f3f98aca705b3c20f7a1e79424701
Stefano Pigozzi <me@steffo.eu>
2022-06-22 05:06:49 +0200

    Improve packaging

--- threadkiller/__main__.py
+++ threadkiller/__main__.py
@@ -6,23 +6,28 @@ from . import config
 from . import handlers
 
 
-config.config.proxies.resolve()
-application = telegram.ext.ApplicationBuilder().token(config.TG_BOT_TOKEN).build()
+def main():
+    config.config.proxies.resolve()
+    application = telegram.ext.ApplicationBuilder().token(config.TG_BOT_TOKEN).build()
 
-application.add_handler(
-    telegram.ext.CommandHandler(
-        "thread",
-        handlers.pin_thread,
-        block=False,
+    application.add_handler(
+        telegram.ext.CommandHandler(
+            "thread",
+            handlers.pin_thread,
+            block=False,
+        )
     )
-)
 
-application.add_handler(
-    telegram.ext.MessageHandler(
-        ~filters.REPLY & filters.ChatType.GROUPS & ~filters.User(user_id=777000) & ~filters.StatusUpdate.ALL & ~filters.COMMAND,
-        handlers.purge,
-        block=False,
+    application.add_handler(
+        telegram.ext.MessageHandler(
+            ~filters.REPLY & filters.ChatType.GROUPS & ~filters.User(user_id=777000) & ~filters.StatusUpdate.ALL & ~filters.COMMAND,
+            handlers.purge,
+            block=False,
+        )
     )
-)
 
-application.run_polling()
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
