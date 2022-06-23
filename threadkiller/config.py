c1a6eb393f53bfabaa6566f0c2eb5ae94f26c5da
Stefano Pigozzi <me@steffo.eu>
2022-06-23 04:45:27 +0200

    Remove unused localization configs

--- threadkiller/config.py
+++ threadkiller/config.py
@@ -1,9 +1,9 @@
 import typing as t
 import cfig
-import enum
 
 
 config = cfig.Configuration()
+
 
 @config.required()
 def TG_BOT_TOKEN(val: str) -> str:
@@ -11,24 +11,6 @@ def TG_BOT_TOKEN(val: str) -> str:
     Token used to authenticate as a Telegram bot.
 
     Obtain one at https://t.me/BotFather .
-    """
-    return val
-
-
-@config.optional()
-def TG_DEFAULT_LANG(val: t.Optional[str]) -> str:
-    """
-    Assume this to be the language of users for whom Telegram is not reporting a language code, or for whom a localization does not exist.
-
-    Defaults to "en" if not specified.
-    """
-    return val or "en"
-
-
-@config.optional()
-def TG_FORCE_LANG(val: t.Optional[str]) -> t.Optional[str]:
-    """
-    If set, assume that all users have this language code, and show them messages in that language.
     """
     return val
 
@@ -51,8 +33,6 @@ def TK_NOTIFY_SECS(val: t.Optional[str]) -> int:
 
 __all__ = (
     "TG_BOT_TOKEN",
-    "TG_DEFAULT_LANG",
-    "TG_FORCE_LANG",
     "TK_NOTIFY_SECS",
 )
 
